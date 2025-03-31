055e9993952220eede102e568f1c274b3f801aca
rhit-yamadaby <yamadaby@rose-hulman.edu>
2025-03-31 13:32:39 -0400

    Add files via upload

---
+++ dance dance twister project.py
@@ -0,0 +1,24 @@
+#dance dance twister code
+#made by Jack Wang, Carter Werkerma, Brian Yamada
+#started on 3/27/2025
+
+#import everything
+
+#main code
+
+# starting 3 blink sequence
+
+# first tile
+
+# second tile
+
+# thrid tile and onward of random tile
+
+
+
+
+# new tile blinking pattern
+
+# move feet blinking pattern
+
+# player messed up patterna
